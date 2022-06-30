243d18fdde28e707823ffb3a003af640d2e5bec9
Onyxymous <aidanwech11@gmail.com>
2022-06-30 10:46:37 -0400

    3 files modified

--- src/app/doc-details/doc-details.component.ts
+++ src/app/doc-details/doc-details.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Doc, docs } from '../docs';
 
 @Component({
   selector: 'app-doc-details',
   templateUrl: './doc-details.component.html',
-  styleUrls: ['./doc-details.component.css']
+  styleUrls: ['./doc-details.component.css'],
 })
 export class DocDetailsComponent implements OnInit {
+  doc: Doc | undefined;
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
+  updateBold(doc: Doc) {
+    var bold = 'normal';
 
-  ngOnInit(): void {
+    if (doc.isBold === true) {
+      bold = 'normal';
+      doc.isBold = false;
+    } else {
+      bold = 'bold';
+      doc.isBold = true;
+    }
+
+    return bold;
   }
 
+  updateItalic(doc: Doc) {
+    var italic = 'normal';
+
+    if (doc.isItalic === true) {
+      italic = 'normal';
+      doc.isItalic = false;
+    } else {
+      italic = 'italic';
+      doc.isItalic = true;
+    }
+
+    return italic;
+  }
+
+  updateUnderline(doc: Doc) {
+    var underlined = 'none';
+
+    if (doc.isUnderlined === true) {
+      underlined = 'none';
+      doc.isUnderlined = false;
+    } else {
+      underlined = 'underline';
+      doc.isUnderlined = true;
+    }
+
+    return underlined;
+  }
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const docNameFromRoute = String(routeParams.get('docName'));
+
+    this.doc = docs.find((doc) => doc.name === docNameFromRoute);
+
+    if (this.doc) {
+      var isBold = this.doc.isBold;
+      var isItalic = this.doc.isItalic;
+      var isUnderlined = this.doc.isUnderlined;
+      var fonttype = this.doc.fonttype;
+      var fontsize = this.doc.fontsize;
+      var fontcolor = this.doc.fontcolor;
+      var highlight = this.doc.highlight;
+    }
+  }
 }
--- src/app/docs.ts
+++ src/app/docs.ts
@@ -1,20 +1,48 @@
 export interface Doc {
   name: string;
   body: string;
+  isBold: boolean;
+  isItalic: boolean;
+  isUnderlined: boolean;
+  fonttype: string;
+  fontsize: number;
+  fontcolor: string;
+  highlight: string;
 }
 
 export const docs = [
   {
     name: 'My Test Document',
     body: 'Hello! If you can read this, you are viewing a document in this Angular program I made.',
+    isBold: false,
+    isItalic: false,
+    isUnderlined: false,
+    fonttype: "Arial",
+    fontsize: 11,
+    fontcolor: "000000",
+    highlight: "ffffff"
   },
   {
     name: 'Wild Doc',
     body: 'Careful! This doc can make those who see it go wild.',
+    isBold: false,
+    isItalic: false,
+    isUnderlined: false,
+    fonttype: "Arial",
+    fontsize: 11,
+    fontcolor: "000000",
+    highlight: "ffffff"
   },
   {
     name: 'random',
     body: '&*^$E%DRYUIO*(JPJI^&*',
+    isBold: false,
+    isItalic: false,
+    isUnderlined: false,
+    fonttype: "Arial",
+    fontsize: 11,
+    fontcolor: "000000",
+    highlight: "ffffff"
   },
 ];
 
