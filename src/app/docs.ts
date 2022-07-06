a1fc1517d5747aa04fd66d8fc913786e27611b4d
Onyxymous <aidanwech11@gmail.com>
2022-07-06 14:08:19 -0400

    3 files modified

--- src/app/docs.ts
+++ src/app/docs.ts
@@ -1,5 +1,6 @@
 export interface Doc {
   name: string;
+  docBarName: string;
   body: string;
   isBold: boolean;
   isItalic: boolean;
@@ -13,35 +14,14 @@ export interface Doc {
 export const docs = [
   {
     name: 'My Test Document',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    docBarName: 'My Test Document',
+    body: '',
     isBold: false,
     isItalic: false,
     isUnderlined: false,
     fonttype: 'Arial',
     fontsize: 11,
     fontcolor: '000000',
-    highlight: 'ffffff',
-  },
-  {
-    name: 'Wild Doc',
-    body: 'Careful! This doc can make those who see it go wild.',
-    isBold: false,
-    isItalic: false,
-    isUnderlined: false,
-    fonttype: 'Arial',
-    fontsize: 11,
-    fontcolor: '000000',
-    highlight: 'ffffff',
-  },
-  {
-    name: 'random',
-    body: '&*^$E%DRYUIO*(JPJI^&*',
-    isBold: false,
-    isItalic: false,
-    isUnderlined: false,
-    fonttype: 'Arial',
-    fontsize: 11,
-    fontcolor: '000000',
-    highlight: 'ffffff',
-  },
+    highlight: 'none',
+  }
 ];
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { Doc, docs } from '../docs';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css'],
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  genDoc() {
+    let newdoc = {} as Doc;
+    let docname = prompt("Name of Document:", "");
+    if (docname != null) {
+      newdoc = {
+        name: docname,
+        docBarName: docname,
+        body: "",
+        isBold: false,
+        isItalic: false,
+        isUnderlined: false,
+        fonttype: "Arial",
+        fontsize: 11,
+        fontcolor: "#000000",
+        highlight: "none"
+      };
+      if (docname.length > 16) {
+        newdoc.docBarName = newdoc.docBarName.substring(0, 13) + "...";
+      }
+      docs.push(newdoc);
+    }
+  }
+}
